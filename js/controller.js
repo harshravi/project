eab66979dbe11651b9cf23442a64b77e35ba222e
unknown <hranjan@AIPL-CHE-LT-248.altimetrik.com>
2016-06-01 10:14:07 +0530

    made some changes in controller.js

--- js/controller.js
+++ js/controller.js
@@ -31,7 +31,10 @@
             
         });
 
-        app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
+
+//        app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
+
+        app.config(['$routeProvider',function($routeProvider){
 		$routeProvider
 			.when('/',
 				{
@@ -51,9 +54,90 @@
 			.otherwise({redirectTO:'/'});
 			 
 //		$locationProvider.html5Mode(true); //Remove the '#' from URL.
-		$locationProvider.html5Mode({
-		  enabled: true,
-		  requireBase: false
-		});
+//		$locationProvider.html5Mode({
+//		  enabled: true,
+//		  requireBase: false
+//		});
 			
 	   }]);
+
+
+		app.controller('entryController',function($scope,$http){
+			
+			
+			$http.get('js/product.json',function(){
+			
+			}).success(function(data){
+//				console.log(JSON.stringify(data));
+				
+				$scope.addproduct = function(){	
+						var j = $scope.product.productId;
+						if(data[j-1].productID == j){
+							$scope.product = data[j-1].properties;
+							$scope.product.productId = data[(j-1)].productID;
+							$scope.product.productQuantity = 1;
+							$scope.product.totalCost = $scope.product.shipingCost;
+						}
+						else{
+							
+							alert("hi");
+						}	
+				}
+
+				$scope.changeQuantity = function(product) {
+
+					$scope.product.totalCost = ($scope.product.shipingCost*$scope.product.productQuantity);		
+				}
+				
+			}).error(function(){
+				console.log("error data not found");
+			});
+			
+
+		});
+
+
+		app.directive('mainArea', function() {
+			return {
+				scope:{},
+				restrict: "C",
+				template: "<div class='col-md-offset-9 col-md-3'>"+
+					"<button type='button' ng-click='appendr()' class='btn btn-primary'>" +
+					"Add New Product" + "</button>" +
+				"</div>",
+				link: function($scope,$element,$attrs) {
+					$scope.appendr = function() {
+						var p = angular.element(document.querySelector( 'tbody.productdatails' ));
+						p.append('<tr ng-bind="productDetail"></tr>');
+					}
+				}
+			}
+		});
+//		app.directive('x',function(){
+//			return{
+//				scope:{},
+//				restrict:'E',
+//				template:'<h1>hello</h1>',
+//				link:function($scope,$element,$attrs){
+//					$element.addClass("box")
+//				}
+//			}
+//		});
+function login(){
+	var useid = document.getElementById(username);
+}
+function register(){
+	var useid = document.getElementById(username);
+}
+function remember(){
+	var username = document.getElementById('username').value;
+	var userpassword = document.getElementById('password').value;
+	if(localStorage){
+		localStorage.setItem('username',username);
+		localStorage.setItem('userpasword',userpassword)
+	}
+	else{
+		alert("localStorage is not supported");
+	}
+	
+}
